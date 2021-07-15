3f5b755e3c82fac2804cf9d2164583ac8a370110
Vitor Piovezan <vitor.piovezan@hotmail.com.br>
2021-07-15 00:17:59 -0300

    search function created
    Search by name function was created

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,27 +5,38 @@ import {
   InputText,
 } from '../style/Styled.Header';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export function Header({ mudaScreen }) {
+  const history = useHistory();
   const [mudaSearch, setMudaSearch] = useState(false);
+  const [search, setSearch] = useState('');
+
+  function SearchTeste() {
+    if (mudaSearch === false) {
+      setMudaSearch(!mudaSearch);
+    } else {
+      history.push('/morebooks/' + search);
+    }
+  }
 
   return (
     <ContainerHeader>
       <ContentHeader widthScreen={`${mudaScreen ? '900px' : '100%'}`}>
         {mudaSearch ? (
-          <InputText placeholder="Buscar por livros ou autores" type="text" />
+          <InputText
+            autoFocus
+            placeholder="Buscar por livros ou autores"
+            type="text"
+            onChange={e => setSearch(e.target.value)}
+          />
         ) : (
           <Link to={'/home'}>
             <img alt="logo google livros" src="/img/logo.png" />
           </Link>
         )}
 
-        <Icon
-          onClick={e => setMudaSearch(!mudaSearch)}
-          color="action"
-          fontSize="large"
-        >
+        <Icon onClick={SearchTeste} color="action" fontSize="large">
           search
         </Icon>
       </ContentHeader>
